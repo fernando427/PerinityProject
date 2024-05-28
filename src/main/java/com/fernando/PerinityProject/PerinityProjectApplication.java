7a8992d1716b0cc014438ce9c428f18066399c1a
Fernando Felipe <fefernando301@hotmail.com>
2024-05-28 19:15:21 -0300

    Initial commit

---
+++ src/main/java/com/fernando/PerinityProject/PerinityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.PerinityProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerinityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerinityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/PerinityProject/PerinityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.PerinityProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerinityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
